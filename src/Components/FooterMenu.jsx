3b5310b85274ffa44d91f1a173248a7827793a1c
rickyspaaanish <rickyspaaanish@gmail.com>
2021-07-30 13:23:20 +0300

    end footer

--- src/Components/FooterMenu.jsx
+++ src/Components/FooterMenu.jsx
@@ -3,26 +3,26 @@ import FooterMenuItem from './FooterMenuItem';
 import NavigationItem from './NavigationItem';
 
 function FooterMenu() {
-  return <div className='footer'><div className='footer__item-logo'><img src={logo} />Linked<span className='blue-text'>In</span></div>
+  return <div className='footer'><div className='footer__item-logo'><img src={logo} /><div className='text__middle text__bold footer__logo-text'>Linked<span className='blue-text'>In</span></div></div>
     <div className='footer__item'>
       <div><span className='list__header text__small text__bold'>Navigation</span>
         <div className='footer__item'>
           <ul className='footer__list'>
-            <li><FooterMenuItem props={['link', 'About']}></FooterMenuItem></li>
-            <li><FooterMenuItem props={['link', 'Careers']}></FooterMenuItem></li>
-            <li><FooterMenuItem props={['link', 'Advertising']}></FooterMenuItem></li>
-            <li><FooterMenuItem props={['link', 'Small Business']}></FooterMenuItem></li>
+            <li className='footer__list-item'><FooterMenuItem props={['link', 'About']}></FooterMenuItem></li>
+            <li className='footer__list-item'><FooterMenuItem props={['link', 'Careers']}></FooterMenuItem></li>
+            <li className='footer__list-item'><FooterMenuItem props={['link', 'Advertising']}></FooterMenuItem></li>
+            <li className='footer__list-item'><FooterMenuItem props={['link', 'Small Business']}></FooterMenuItem></li>
           </ul>
           <ul className='footer__list'>
-            <li><FooterMenuItem props={['link', 'Talent Solutions']}></FooterMenuItem></li>
-            <li><FooterMenuItem props={['link', 'Marketing Solutions']}></FooterMenuItem></li>
-            <li><FooterMenuItem props={['link', 'Sales Solutions']}></FooterMenuItem></li>
-            <li><FooterMenuItem props={['link', 'Safety Center']}></FooterMenuItem></li>
+            <li className='footer__list-item'><FooterMenuItem props={['link', 'Talent Solutions']}></FooterMenuItem></li>
+            <li className='footer__list-item'><FooterMenuItem props={['link', 'Marketing Solutions']}></FooterMenuItem></li>
+            <li className='footer__list-item'><FooterMenuItem props={['link', 'Sales Solutions']}></FooterMenuItem></li>
+            <li className='footer__list-item'><FooterMenuItem props={['link', 'Safety Center']}></FooterMenuItem></li>
           </ul>
           <ul className='footer__list'>
-            <li><FooterMenuItem props={['link', 'Community Guidelines']}></FooterMenuItem></li>
-            <li><FooterMenuItem props={['link', 'Privacy & Terms']}></FooterMenuItem></li>
-            <li><FooterMenuItem props={['link', 'Mobile App']}></FooterMenuItem></li>
+            <li className='footer__list-item'><FooterMenuItem props={['link', 'Community Guidelines']}></FooterMenuItem></li>
+            <li className='footer__list-item'><FooterMenuItem props={['link', 'Privacy & Terms']}></FooterMenuItem></li>
+            <li className='footer__list-item'><FooterMenuItem props={['link', 'Mobile App']}></FooterMenuItem></li>
           </ul>
         </div>
       </div>
--- src/Components/FooterMenuItem.jsx
+++ src/Components/FooterMenuItem.jsx
@@ -5,8 +5,10 @@ function FooterMenuItem({ props }){
         case 'button':
             return <button className={'footer__button ' + props[1]}>{props[2].toUpperCase()}<span className={props[3]}></span></button>;
         case 'select':
-            return <select>
-                { props[1].forEach(element => {<option>{element.toUpperCase()}</option>}) }
+            const options = [];
+            props[1].forEach(element => {options.push(<option className='text__small text__bold'>{element.toUpperCase()}</option>)});
+            return <select className='language__select text__small text__bold'>
+                {options}
             </select>
     }
 }
