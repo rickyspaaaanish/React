3a0b237d488e6294a92cbedf186992bc6f867bee
rickyspaaanish <rickyspaaanish@gmail.com>
2021-08-03 10:50:36 +0300

    +fix

--- src/Components/MainContentBlock.jsx
+++ src/Components/MainContentBlock.jsx
@@ -5,7 +5,7 @@ function MainContentBlock({blocksInfo}) {
     <div className='sort__selection-block'><div className='border-line'></div><div className='sort-control text__small text__bold'>{('Sort by: ').toUpperCase()}<select className='sorting-select text__small text__bold blue-text'>
       <option>{('trending').toUpperCase()}</option><option>{('latest').toUpperCase()}</option></select></div><div className='border-line'></div></div>
     <div className='Main__content-block'>
-      <div className='block__header-info text__xsmall'>{blocksInfo[0].header}<div className='icon-more-horizontal'></div></div>
+      <div className='block__header-info text__xsmall'>{blocksInfo[0].header}<div style={{'font-size': "22px"}} className='icon-more-horizontal'></div></div>
       <div><div className='block__user-info'><img src={blocksInfo[0].userpic}/><div class='block__user-text-info'><span className='text__bold'>{blocksInfo[0].username}</span>
       <span className='text__xsmall'>{blocksInfo[0].userpos}</span></div></div>
       <div className='block__content text__middle'><span>{blocksInfo[0].content}</span><div className='expand-control'>read more</div></div>
