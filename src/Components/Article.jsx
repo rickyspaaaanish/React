74fc2671404076ec25f5749983c34f4214b7efd9
rickyspaaanish <rickyspaaanish@gmail.com>
2021-08-09 00:57:23 +0300

    +fix

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -2,10 +2,8 @@ function Article({ params }) {
   return (
     <div className="aricle">
       <img src={params.img} alt="" />
-      <div className='article__desription'>
-        <span className="text__middle text__bold">
-          {params.name}
-        </span>
+      <div className="article__desription">
+        <span className="text__middle text__bold">{params.name}</span>
         <span className="text__xsmall">
           {params.viewers.toLocaleString("en-IN", {
             maximumSignificantDigits: 3,
